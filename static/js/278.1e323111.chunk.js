94f5078c4d4a7407aca498f5b2dfb042661453d0
SvistPetro <119351964+SvistPetro@users.noreply.github.com>
2024-02-10 19:20:21 +0000

    Deploying to gh-pages from @ SvistPetro/goit-react-hw-05-movies@18ff460d176805c5529ddfa82eff04add02bcce4 🚀

---
+++ static/js/278.1e323111.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[278],{278:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Movies page SviP"})}}}]);
+//# sourceMappingURL=278.1e323111.chunk.js.map
